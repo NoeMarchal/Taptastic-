f6cd922ae030b8330bbac0f98ffc5c26b224eb24
NoeMarchal <noemarchal59@icloud.com>
2025-02-06 20:02:48 +0100

    avtr

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let upgrade1Level = 0;
 let upgrade2Level = 0;
 let unlockedTrophies = []; // Liste des trophées débloqués
 let playerName = "Nom du joueur"; // Nom par défaut
-let avatarSrc = "images/choose_avatar.jpg"; // Avatar par défaut
+let avatarSrc = "Images/choose_avatar.jpg"; // Avatar par défaut
 
 // Liste des trophées et leurs conditions
 const trophies = [
@@ -183,7 +183,7 @@ function changeAvatar(avatarFileName) {
     const avatarImg = document.getElementById("avatar");
 
     // Mettre à jour l'image
-    const newAvatarPath = `images/${avatarFileName}`;
+    const newAvatarPath = `Images/${avatarFileName}`;
     avatarImg.src = newAvatarPath;
 
     // Sauvegarder l'avatar sélectionné
@@ -259,7 +259,7 @@ function resetGame() {
     upgrade1Level = 0;
     upgrade2Level = 0;
     playerName = "Nom du joueur";
-    avatarSrc = "images/choose_avatar.jpg";
+    avatarSrc = "Images/choose_avatar.jpg";
     unlockedTrophies = [];  // Réinitialise les trophées
 
     // Supprime la sauvegarde complète et recharge le jeu
