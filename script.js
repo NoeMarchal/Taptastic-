b067bd9209df783d1f47bd7c64b0452828a87cd1
NoeMarchal <noemarchal59@icloud.com>
2025-02-07 10:02:04 +0100

    trophies value

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ let avatarSrc = "Images/choose_avatar.jpg"; // Avatar par défaut
 // Liste des trophées et leurs conditions
 const trophies = [
     { name: "Débutant", condition: 100 },
-    { name: "Apprenti Clicker", condition: 10000 },
-    { name: "Clicker Amateur", condition: 50000 },
-    { name: "Clicker Confirmé", condition: 100000 },
-    { name: "Clicker Pro", condition: 500000 },
-    { name: "Maître du Click", condition: 1000000 }
+    { name: "Apprenti Clicker", condition: 100000 },
+    { name: "Clicker Amateur", condition: 500000 },
+    { name: "Clicker Confirmé", condition: 1000000 },
+    { name: "Clicker Pro", condition: 5000000 },
+    { name: "Maître du Click", condition: 10000000 }
 ];
 
 // Éléments du DOM
